eb97f102c407366cfb3acdf2255d79b6f2014cf8
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2026-04-01 10:07:19 -0700

    Fix statements spec
    The spec was creating records using an scoped relation, assuming the relation would create records using the conditions from the scope. However, the scope applied an array condition for the `operation` column, so no single value could be used.
    
    Instead, we switch this to an explicit insert operation, so we know the type of CommitEntry that will be created for the test.

--- spec/lib/schema/statements_spec.rb
+++ spec/lib/schema/statements_spec.rb
@@ -8,7 +8,7 @@ describe Stagehand::Schema::Statements do
       ActiveRecord::Schema.define { drop_table('doodads') }
     end
 
-    let(:entry) { Stagehand::Staging::CommitEntry.save_operations.create!(:table_name => 'widgets', :record_id => 1) }
+    let(:entry) { Stagehand::Staging::CommitEntry.insert_operations.create!(:table_name => 'widgets', :record_id => 1) }
 
     it 'updates the table name column of commit entries for the given table' do
       create_table('widgets', :stagehand => true)
